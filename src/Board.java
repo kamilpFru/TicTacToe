a14115e7d6227c3c341f4f72f3ccc301b63a9117
kamilpFru <114623889+kamilpFru@users.noreply.github.com>
2022-11-27 02:32:22 +0100

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -5,9 +5,8 @@ public class Board {
                                     {' ', ' ', ' '}};
 
     public final char[][] helpingBoard = {{'1', '2', '3'},
-                                           {'4', '5', '6'},
-                                           {'7', '8', '9'}};
-
+                                          {'4', '5', '6'},
+                                          {'7', '8', '9'}};
 
     public boolean isTaken(int x) {
         return !(getField(x) == ' ');
